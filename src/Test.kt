795b2ec6825a592aff7a92181ca97f124980709e
GlicH-ctrl <strenindenis@gmail.com>
2026-02-27 19:16:03 +0300

    Add data class examples

--- src/Test.kt
+++ src/Test.kt
@@ -1,10 +1,16 @@
-class User {
-    var age: Int = 0
-        set(value) {
-            if (value in 0..150) {
-                field = value
-            } else {
-                println("Ошибка: возраст должен быть от 0 до 150")
-            }
-        }
+data class Item(val id: Int, val name: String, var quantity: Int) {
+    override fun toString(): String {
+        return "Предмет: $name | ID: $id | Количество: $quantity"
+    }
+}
+
+fun main() {
+    val item1 = Item(1, "Меч", 5)
+    val item2 = item1.copy(quantity = 10)
+
+    println(item1)
+    println(item2)
+
+    val (id, name, quantity) = item1
+    println("Декомпозиция: id=$id, name=$name, quantity=$quantity")
 }
